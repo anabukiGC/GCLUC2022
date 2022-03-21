9168a0511ff66f451b4b29a21a753fa12bd436d9
okafuta <21012104@c.anabuki-college.net>
2022-03-21 22:58:21 +0900

    debahu

--- Project/Project/GameProject/Game/UI/Debuff.cpp
+++ Project/Project/GameProject/Game/UI/Debuff.cpp
@@ -4,6 +4,7 @@ Debuff::Debuff(const CVector2D& pos) :Base(eType_UI, 11)
 {
 	m_pos = pos;
 	m_img = COPY_RESOURCE("Debuff", CImage);
+	m_img.SetSize(32,32);
 }
 void Debuff::Update()
 {
@@ -12,7 +13,8 @@ void Debuff::Update()
 
 void Debuff::Draw()
 {
-
+	CVector2D pos2D = Get2DPos(m_pos);
+	m_img.SetPos(pos2D);
 	m_img.Draw();
 }
 
